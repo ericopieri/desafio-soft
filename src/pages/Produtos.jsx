c2a5c1770b687e34b08f648e0a6eb7c6f485fba7
ericopieri <erico.pieri@gmail.com>
2023-03-02 08:43:11 -0300

    finalizado post novo produto

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -12,6 +12,35 @@ function Produtos() {
     const [newProduto, setNewProduto] = useState({});
     const [tipos, setTipos] = useState([]);
 
+    const postNewProduto = async () => {
+        try {
+            const newProdutoParams = new URLSearchParams();
+            newProdutoParams.append("nome", newProduto.nome);
+            newProdutoParams.append("valor", newProduto.valor);
+            newProdutoParams.append("tipo", newProduto.tipo);
+
+            const { data } = await axios.post("http://locahost/", newProduto);
+            setProdutos([...produtos, data]);
+            setNewProduto({});
+            setShowForm(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const changeInput = (event) => {
+        setNewProduto((produto) => ({
+            ...newProduto,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    const changeSelect = (codigo) => {
+        console.log(codigo);
+    };
+
+    useEffect(() => console.log(newProduto), [newProduto]);
+
     useEffect(() => {
         const getProdutos = async () => {
             const { data } = await axios.get(
@@ -23,7 +52,7 @@ function Produtos() {
 
         const getTipos = async () => {
             const { data } = await axios.get(
-                "http://localhost/Desafio/tipoProduto.php"
+                "http://localhost/Desafio/produtoPost.php"
             );
 
             setTipos(data);
@@ -32,10 +61,6 @@ function Produtos() {
         getProdutos();
         getTipos();
     }, []);
-
-    useEffect(() => {
-        console.log(produtos);
-    }, [produtos]);
 
     return (
         <div className="conteudo-principal">
@@ -51,24 +76,30 @@ function Produtos() {
                 </section>
                 {showForm && (
                     <FormCadastro
+                        handleSubmit={postNewProduto}
                         className="produtos-form"
                         title="Cadastro de um novo Produto"
                     >
                         <Input
+                            handleChange={changeInput}
                             text="Nome"
                             type="text"
                             placeholder="Nome do Produto"
                             name="nome"
                             className="cadastro-input"
+                            value={newProduto.nome || null}
                         />
                         <Input
+                            value={newProduto.valor || null}
+                            handleChange={changeInput}
                             text="Valor"
                             type="number"
                             placeholder="Valor unitário"
-                            name="nome"
+                            name="valor"
                             className="cadastro-input"
                         />
                         <Select
+                            handleChange={changeSelect}
                             text="Tipo"
                             options={tipos}
                             placeholder="Tipo do Produto"
