665e7cd0f4e3ae94101856f2de55055ac6d72583
ericopieri <erico.pieri@gmail.com>
2023-03-08 09:16:17 -0300

    remover item funcional

--- src/components/venda/LinhaTabela.jsx
+++ src/components/venda/LinhaTabela.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 
-function LinhaTabela({ item, handleClick, excluding }) {
+function LinhaTabela({ item, handleClickRemove }) {
     return (
-        <tr
-            onClick={() => {
-                console.log(item.id);
-                handleClick(item.id);
-            }}
-            className={excluding !== false ? "exclude" : ""}
-        >
+        <tr>
             <td className="left-text">
                 {item.produto.codigo} - {item.produto.nome}
             </td>
@@ -16,7 +10,12 @@ function LinhaTabela({ item, handleClick, excluding }) {
             <td>R$ {item.produto.valor.toFixed(2)}</td>
             <td>R$ {item.total.toFixed(2)}</td>
             <td>
-                <button className="button-ex-item">Excluir</button>
+                <button
+                    className="button-ex-item"
+                    onClick={() => handleClickRemove(item.id)}
+                >
+                    Excluir
+                </button>
             </td>
         </tr>
     );
