e4ba960abd6fe06b6944c984ba260d1c615d1c17
ericopieri <erico.pieri@gmail.com>
2023-03-10 16:11:59 -0300

    produto deletado em item venda

--- src/components/relatorio/ItemVendaCard.jsx
+++ src/components/relatorio/ItemVendaCard.jsx
@@ -1,22 +1,29 @@
 import React from "react";
+import moment from "moment";
 
 function ItemVendaCard({ item }) {
+    const checkDeletado = () => {
+        if (item.produto_deletadoEm) {
+            return " deletado";
+        }
+
+        return "";
+    };
+
     const imposto =
-        item.quantidade *
-        item.produto.valor *
-        (item.produto.tipo.percentual_imposto / 100);
+        item.quantidade * item.produto_valor * (item.percentual_imposto / 100);
 
     return (
-        <div className="item-venda-card">
+        <div className={"item-venda-card" + checkDeletado()}>
             <h4>
-                {item.produto.nome}{" "}
+                {item.produto_nome}{" "}
                 <span className="quantidade-item-venda">
                     ({item.quantidade}x)
                 </span>
             </h4>
             <p className="preco-item-venda">
                 Valor un.:{"  "}
-                <span>R$ {item.produto.valor.toFixed(2)}</span>
+                <span>R$ {item.produto_valor.toFixed(2)}</span>
             </p>
             <div className="total-item-venda">
                 Total: <span>R$ {item.total.toFixed(2)}</span>
@@ -24,6 +31,15 @@ function ItemVendaCard({ item }) {
             <div className="imposto-item-venda">
                 Imposto: <span>R$ {imposto.toFixed(2)}</span>
             </div>
+            {item.produto_deletadoEm && (
+                <p className="deletado-p">
+                    Produto <span>deletado</span> do sistema no dia{" "}
+                    {moment(item.produto_deletadoEm).format(
+                        "DD [de] MMMM [de] YYYY"
+                    )}{" "}
+                    às {moment(item.produto_deletadoEm).format("HH[h]mm")}
+                </p>
+            )}
         </div>
     );
 }
